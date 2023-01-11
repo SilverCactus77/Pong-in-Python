3e2f6cb086fd65f39c1a618cc26400eaf65c7fa5
noahsali <noahsali@woodland.on.ca>
2023-01-11 11:55:03 -0500

    pong in python

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ window.bgcolor("blue")
 window.setup(width=800, height=600)
 window.tracer(0)
 
-#Creating the left paddle
-leftpaddle=t.Turtle()
-leftpaddle.speed(0)
-leftpaddle.shape("square")
-leftpaddle.color("white")
-leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
-leftpaddle.penup()
-leftpaddle.goto(-350,0)
+# Creating the left paddle
+lp = t.Turtle()
+lp.speed(0)
+lp.shape("square")
+lp.color("white")
+lp.shapesize(stretch_wid=5, stretch_len=1)
+lp.penup()
+lp.goto(-350, 0)
+
+# Creating the right paddle
+rp = t.Turtle()
+rp.speed(0)
+rp.shape("square")
+rp.color("White")
+rp.shapesize(stretch_wid=5, stretch_len=1)
+rp.penup()
+rp.goto(350, 0)
+
