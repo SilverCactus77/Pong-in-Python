f3fc7285bd5ed04d1949957777e03b6e39034f32
noahsali <noahsali@woodland.on.ca>
2023-01-11 11:11:33 -0500

    pong in python

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import turtle as t
 
+playerAscore = 0
+playerBscore = 0
+
+# create a window and declare a variable called window and call the screen()
+window = t.Screen()
+window.title("Pong")
+window.bgcolor("blue")
+window.setup(width=800, height=600)
+window.tracer(0)
+
