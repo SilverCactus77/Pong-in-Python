566788e2f626b1e696c2a983c3958320976c6241
noahsali <noahsali@woodland.on.ca>
2023-01-12 14:42:34 -0500

    pong in python

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import turtle as t
 
+# Player score at the beginning of the game
 playerAscore = 0
 playerBscore = 0
 
-# create a window and declare a variable called window and call the screen()
+# Create a window for the game and import screen form turtle
 window = t.Screen()
 window.title("Pong")
 window.bgcolor("blue")
 window.setup(width=800, height=600)
 window.tracer(0)
 
-# Creating the left paddle
+# Creates the left paddle
 lp = t.Turtle()
 lp.speed(0)
 lp.shape("square")
@@ -19,7 +20,7 @@ lp.shapesize(stretch_wid=5, stretch_len=1)
 lp.penup()
 lp.goto(-350, 0)
 
-# Creating the right paddle
+# Creates the right paddle
 rp = t.Turtle()
 rp.speed(0)
 rp.shape("square")
@@ -27,4 +28,17 @@ rp.color("White")
 rp.shapesize(stretch_wid=5, stretch_len=1)
 rp.penup()
 rp.goto(350, 0)
+
+# Creates the ball
+ball = t.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("black")
+ball.penup()
+ball.goto(5, 5)
+ballxdirection=0.2
+ballydirection=0.2
+
+
+
 
