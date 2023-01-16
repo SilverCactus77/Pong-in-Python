1c54fb7054aec22e9dd2d910993b994d3acd77b3
noahsali <noahsali@woodland.on.ca>
2023-01-16 10:22:16 -0500

    pong in python

--- main.py
+++ main.py
@@ -42,7 +42,88 @@ ballydirection=0.2
 # This code makes a display for player scores
 display = t.Turtle()
 display.speed(0)
-display
+display.color("blue")
+display.penup()
+display.hideturtle()
+display.goto(0, 260)
+display.write("score", align="center", font=('Arial', 24, 'normal'))
+
+# code for moving left paddle
+def lpup():
+    y = lp.ycor()
+    y = y + 90
+    lp.sety(y)
+
+def lpdown():
+    y = lp.ycor()
+    y = y + 90
+    lp.sety(y)
+
+# code for moving right paddle
+def rpup():
+    y = rp.ycor()
+    y = y + 90
+    rp.set(y)
+
+def rpdown():
+    y = rp.ycor()
+    y = y + 90
+    rp.set(y)
+
+# Assign controls to play
+window.listen()
+window.onkeypress(lpup, 'w')
+window.onkeypress(lpdown, 's')
+window.onkeypress(rpup, 'Up')
+window.onkeypress(rpdown, 'Down')
+
+while True:
+    window.update()
+
+    # moving the ball
+    ball.setx(ball.xcor() + ballxdirection)
+    ball.sety(ball.ycor() + ballxdirection)
+
+# setting up a border
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ballydirection = ballydirection*-1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ballydirection = ballydirection*-1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball_dx = ball_dx * -1
+        player_a_score = player_a_score + 1
+        display.clear()
+        display.write("Player A: {}  Player B: {} ".format(player_a_score, player_b_score),
+                  align="center", font=('Monaco', 24, "normal"))
+        os.system("afplay wallhit.wav&")
+
+    if(ball.xcor()) < -390:
+        # Left width paddle Border
+        ball.goto(0,0)
+        ball_dx = ball_dx * -1
+        player_b_score = player_b_score + 1
+        display.clear()
+        display.write("Player A: {}  Player B: {} ".format(player_a_score, player_b_score), align="center", font=('Monaco', 24, "normal"))
+        os.system("afplay wallhit.wav&")
+
+# Handling the collisions with paddles.
+
+    if (ball.xcor() > 340) and (ball.xcor() < 350) and (
+            ball.ycor() < rp.ycor() + 40 and ball.ycor() > rp.ycor() - 40):
+        ball.setx(340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
+
+    if (ball.xcor() < -340) and (ball.xcor() > -350) and (
+            ball.ycor() < lp.ycor() + 40 and ball.ycor() > lp.ycor() - 40):
+        ball.setx(-340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
+
 
 
 
